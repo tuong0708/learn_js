d00eb245c1cdffca564ad0c5241d672aa292d31a
nguyen manh tuong <tuong078123@gmail.com>
2021-07-30 21:28:04 +0700

    excercise1.js

---
+++ excercise1.js
@@ -0,0 +1,138 @@
+// có 3 quả cam trong rổ nam ăn hết 2 quả;
+// xe chở cam có 15 quả cam làm rơi mất 5 quả;
+// (số nguyên)
+
+var orange_trong_ro = 3;
+orange_trong_ro = orange_trong_ro - 2;
+var orange_tren_xe = 15;
+orange_tren_xe = orange_tren_xe - 5;
+var sum_orange = orange_trong_ro + orange_tren_xe;
+console. log(sum_orange);
+
+
+// 1 ly nước đựng đc 67,23 ml nước, đổ nước trào mất 23,44 ml;
+// bình nước chứa đc 120,20 ml nước, bình làm đổ mất 24,34 ml;
+// (số thập phân)
+   
+var coc_nuoc = 67.23;
+coc_nuoc = coc_nuoc - 23.44;
+var binh_nuoc = 120.20;
+binh_nuoc = binh_nuoc  - 24.34;
+var ml_nuoc = coc_nuoc + binh_nuoc;
+console. log(ml_nuoc);
+
+
+// string
+var Age = 13;
+var phone = 0931566780;
+var address = "tổ 14c khu 5b quang trung uông bí quảng ninh";
+var description = "đẹp trai nhất vũ trụ =))";
+
+// boolean
+var
+
+
+
+// const(hằng số);
+// số lượng học sinh cao nhất trong trường là 1980;
+// số lượng trái cây cao nhất trong 1 thùng là 58;
+// giá sách này chỉ chịu được 90kg là cao nhất;
+// con đường này có chiều cao an toàn là 14m;
+const MAX_People = 1980;
+console. log(MAX_People);
+const MAX_Fruit_in_the_box = 58;
+console. log(MAX_Fruit_in_the_box);
+const MAX_KG = 90;
+console. log(MAX_KG);
+const MAX_M = 14;
+console. log(MAX_M);
+
+
+
+
+
+// +,-,*,/,%;
+
+
+// số nguyên;
+var x = 4;
+var y = 6;
+console. log(y - x);
+// số thập phân;
+var x = 4.6
+var y = 2.3
+console. log(x - y);
+
+
+
+// số nguyên;
+var x = 6;
+var y = 3;
+console.log(x + y);
+// số thập phân;
+var x = 60.4
+var y = 92.6
+console. log(x + y);
+
+
+
+// số nguyên;
+var x = 4;
+var y = 2;
+console. log(4 * 2);
+// số thập phân
+var x = 57.5;
+var y = 12.4;
+console. log(x * y);
+
+
+// số nguyên
+var x = 10;
+var y = 5;
+console. log(x / y);
+// số thập phân
+var x = 35.2;
+var y = 23.9;
+console. log(x / y);
+
+// số nguyên
+var x = 60;
+var y = 40;
+console. log(x % y);
+// só thập phân
+var x = 27.6;
+var y = 14.6;
+console. log(x % y);
+
+
+// nối 2 biến string;
+var x = 'tuong';
+var y = "handsome";
+var c = x + '' + y;
+console. log(c);
+
+
+
+// gán
+var a = 5993;
+var b = a;
+console. log(b);
+
+
+// ++, --;
+var x = 3;
+x++;
+console. log(x);
+
+var d = 54;
+d--;
+console. log(d);
+
+// tăng 10 lần và giảm 5 lần
+var t = 30;
+t +=10;
+console. log(t);
+
+var y = 42;
+y -=5;
+console. log(y);
